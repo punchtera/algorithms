0a0cd0130bd305f5fe879a368394135335f47809
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2021-12-23 12:30:31 -0500

    add expected behavior to transform array

---
+++ src/transformArray.ts
@@ -0,0 +1,17 @@
+export function transformArray(s: string) {
+    let numberOfTimesCharacter = 1;
+    let characterPointer = s[0];
+
+    let response = []
+    for (let index = 1; index <= s.length; index++) {
+        if (characterPointer === s[index]) {
+            numberOfTimesCharacter += 1
+        } else {
+            response.push({ [characterPointer]: numberOfTimesCharacter });
+            numberOfTimesCharacter = 1;
+            characterPointer = s[index]
+        }
+    }
+
+    return response;
+}
---
+++ test/transformArray.spec.ts
@@ -0,0 +1,8 @@
+import { transformArray } from "../src/transformArray";
+describe("transform array function", () => {
+    it("should return the expected value", () => {
+        const givenString = "aaaabbbcca"
+        const expectedOutput = [{ "a": 4 }, { "b": 3 }, { "c": 2 }, { "a": 1 }]
+        expect(transformArray(givenString)).toEqual(expectedOutput);
+    });
+});
