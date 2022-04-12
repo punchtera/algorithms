63c87528009f48d47cb22b21c8dbdc150704c6de
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-04-12 11:15:32 -0500

    adding two sum scenario and function

--- src/twoSum.ts
+++ src/twoSum.ts
@@ -1,3 +1,28 @@
-export function twoSum(arr: number[], target: number) {
-    return true
+function naiveTwoSum(arr: number[], target: number) {
+
+    const arrLength = arr.length
+
+    for (let i = 0; i < arrLength; i++) {
+
+        for (let j = i + 1; j < arrLength; j++) {
+            if (arr[i] == target - arr[j]) {
+                return true
+            }
+
+        }
+    }
+    return false
 }
+
+function setTwoSum(arr: number[], target: number) {
+    const setToEval = new Set(arr)
+
+    for (let el of setToEval) {
+        if (setToEval.has(target - el)) {
+            return true
+        }
+    }
+    return false
+}
+
+export { setTwoSum }
--- test/twoSum.spec.ts
+++ test/twoSum.spec.ts
@@ -1,17 +1,33 @@
-import { twoSum } from "../src/twoSum";
+import { setTwoSum } from "../src/twoSum";
+
 describe("test add function", () => {
+
+    // Ask if the array is sorted.
+    // Is there a constraint in memory usage?
+    // Is there any constraint at runtime?
+
     it("should return true as 5 and 2 inside the array sum 7", () => {
 
         const givenArr = [3, 1, 5, 2, 9]
         const givenTarget = 7
 
-        expect(twoSum(givenArr, givenTarget)).toBe(true);
+        expect(setTwoSum(givenArr, givenTarget)).toBe(true);
     });
+
     it("should return false as none of the pairs inside the array return 1", () => {
 
         const givenTarget = 1
         const givenArr = [3, 1, 5, 2, 9]
 
-        expect(twoSum(givenArr, givenTarget)).toBe(false);
+        expect(setTwoSum(givenArr, givenTarget)).toBe(false);
     });
+
+    it("should return true as none of the pairs inside the array return 7", () => {
+
+        const givenTarget = 7
+        const givenArr = [3, 5, 2, -4, 8, 11]
+
+        expect(setTwoSum(givenArr, givenTarget)).toBe(true);
+    });
+
 });
