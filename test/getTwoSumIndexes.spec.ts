edbce5f607a0ff93e7302ddfe440a6030993e3be
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-04-13 10:08:17 -0500

    add definition for test case

--- test/getTwoSumIndexes.spec.ts
+++ test/getTwoSumIndexes.spec.ts
@@ -2,31 +2,27 @@ import { getTwoSumIndexes } from "../src/getTwoSumIndexes";
 
 describe("test two sum index function", () => {
 
-    // Ask if the array is sorted.
-    // Is there a constraint in memory usage?
-    // Is there any constraint at runtime?
-
-    it("should return true as 5 and 2 inside the array sum 7", () => {
+    it("should return [2, 3] for 5 & 2", () => {
 
         const givenArr = [3, 1, 5, 2, 9]
         const givenTarget = 7
 
-        expect(getTwoSumIndexes(givenArr, givenTarget)).toBe([2, 3]);
+        expect(getTwoSumIndexes(givenArr, givenTarget)).toEqual([2, 3]);
     });
 
-    it("should return false as none of the pairs inside the array return 1", () => {
+    it("should return [] as none of the pairs inside the array return 1", () => {
 
         const givenTarget = 1
         const givenArr = [3, 1, 5, 2, 9]
 
-        expect(getTwoSumIndexes(givenArr, givenTarget)).toBe(false);
+        expect(getTwoSumIndexes(givenArr, givenTarget)).toEqual([]);
     });
 
-    it("should return true as none of the pairs inside the array return 7", () => {
+    it("should return [[1, 2], [4, 5]] as there are two pairs that meet the criteria", () => {
 
         const givenTarget = 7
         const givenArr = [3, 5, 2, -4, 8, 11]
 
-        expect(getTwoSumIndexes(givenArr, givenTarget)).toBe(true);
+        expect(getTwoSumIndexes(givenArr, givenTarget)).toEqual([[1, 2], [4, 5]]);
     });
 });
