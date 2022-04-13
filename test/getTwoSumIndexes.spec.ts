d751613662573121d15d42f61eab11b95414bca5
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-04-13 10:58:58 -0500

    update test case

--- test/getTwoSumIndexes.spec.ts
+++ test/getTwoSumIndexes.spec.ts
@@ -18,11 +18,11 @@ describe("test two sum index function", () => {
         expect(getTwoSumIndexes(givenArr, givenTarget)).toEqual([]);
     });
 
-    it("should return [[1, 2], [4, 5]] as there are two pairs that meet the criteria", () => {
+    it("should return [[1, 2], [3, 5]] as there are two pairs that meet the criteria", () => {
 
         const givenTarget = 7
         const givenArr = [3, 5, 2, -4, 8, 11]
 
-        expect(getTwoSumIndexes(givenArr, givenTarget)).toEqual([[1, 2], [4, 5]]);
+        expect(getTwoSumIndexes(givenArr, givenTarget)).toEqual([[1, 2], [3, 5]]);
     });
 });
