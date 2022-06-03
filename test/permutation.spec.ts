288fa1f628df22de2081d0645057db298652e5ec
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-06-03 16:42:50 -0500

    fix unit test

--- test/permutation.spec.ts
+++ test/permutation.spec.ts
@@ -2,7 +2,11 @@ import { permutation } from "../src/permutation";
 describe("test permutation function", () => {
     it("should call recursively for 'Juan'", () => {
         const givenStr = 'Juan';
+        const expectedNumberOfCalls = 4*3*2*1;
+        console.log = jest.fn();
+
         permutation(givenStr);
-        expect(true).toEqual(true);
+
+        expect(console.log).toHaveBeenCalledTimes(expectedNumberOfCalls);
     });
 });
