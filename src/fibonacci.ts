456d45b68a7a62b8d033705bbff294298831900c
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-06-30 15:01:43 -0500

    add recursion solution to fibonacci

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,4 +1,4 @@
-export function calculateFibonacciSequence(){
+export function calculateFibonacciSequence() {
     const result = [0, 1];
     let acc = 1;
     let prev = 1;
@@ -6,9 +6,19 @@ export function calculateFibonacciSequence(){
     for (let i = 0; i < 8; i++) {
         result.push(acc)
         acc += prev
-        prev = result[2+i]
+        prev = result[2 + i]
     }
 
     return result
 }
 
+export function calculateFibonacciSequenceRecursively(arr: number[] = [0, 1]): number[] {
+    if (arr.length === 10) {
+        return arr
+    } else {
+        const last = arr[arr.length - 1]
+        const previous = arr[arr.length - 2]
+        arr.push(last + previous)        
+        return calculateFibonacciSequenceRecursively(arr)
+    }
+}
--- test/fibonacci.spec.ts
+++ test/fibonacci.spec.ts
@@ -1,6 +1,14 @@
-import { calculateFibonacciSequence } from "../src/fibonacci";
+import { calculateFibonacciSequence, calculateFibonacciSequenceRecursively } from "../src/fibonacci";
+
 describe("test calculateFibonacciSequence function", () => {
+
   it("should return the first 10 elements of the fibonacci sequence", () => {
     expect(calculateFibonacciSequence().length).toEqual(10);
   });
+
+  it("should return the first 10 elements after calling the funcion recursevily", () => {
+    // This is the example of the first 10 elements of the fibonacci sequence
+    // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+    expect(calculateFibonacciSequenceRecursively().length).toEqual(10)
+  })
 });
