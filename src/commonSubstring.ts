41d1f22c125407ca3cd5df5659d8024f876e013d
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-07-05 10:47:37 -0500

    add basic structure for testing commonsubstring

---
+++ src/commonSubstring.ts
@@ -0,0 +1,17 @@
+function commonSubstring(a: string, b: string) {
+    let biggerIndex = Number.MIN_SAFE_INTEGER
+    
+    if(a.length > b.length){
+
+    }
+
+    if(b.length > a.length){
+
+    }
+
+    return 3;
+}
+
+export { commonSubstring }
+
+
---
+++ test/commonSubstring.spec.ts
@@ -0,0 +1,11 @@
+import { commonSubstring } from "../src/commonSubstring";
+
+describe("test commonSubstring function", () => {
+    it("should return 4 as a result from 'GORI' length", () => {
+        const a = 'ALGORITHM';
+        const b = 'PLÅGORIS';
+
+        const expectedCommonSubstring = 'GORI';
+        expect(commonSubstring(a, b)).toBe(expectedCommonSubstring.length);
+    });
+});
