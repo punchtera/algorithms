80e37399717e6159a4fb06b4732a3836e93eee4a
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-07-21 19:17:47 -0500

    solve the test case

--- src/pairInteger.ts
+++ src/pairInteger.ts
@@ -1,3 +1,25 @@
-export function calculatePairInteger(arr: number[], diff: number): [] {
-    return []
+// The complexity of this function is O(logn) as the value of the internal foreach 
+// is decreasing over time because of the external one 
+
+export function calculatePairInteger(arr: number[], diff: number): {}[] {
+
+    const response: {}[] = [];
+
+    for (let iindex = 0; iindex < arr.length; iindex++) {
+        const element = arr[iindex];
+        for (let jindex = 1 + iindex; jindex < arr.length; jindex++) {
+            const innerElement = arr[jindex];
+
+            const actualDiff = Math.abs(element - innerElement);
+            if (actualDiff === diff && element > innerElement) {
+                response.push({ [innerElement]: [element] });
+            }
+            if (actualDiff === diff && innerElement > element) {
+                response.push({ [element]: [innerElement] });
+            }
+
+        }
+    }
+
+    return response;
 }
--- test/pairInteger.spec.ts
+++ test/pairInteger.spec.ts
@@ -1,13 +1,14 @@
 import { calculatePairInteger } from "../src/pairInteger";
 
-describe("test calculateFibonacciSequence function", () => {
+describe("test calculatePairInteger function", () => {
 
-  it("should return the first 10 elements of the fibonacci sequence", () => {
+  it("should return the elements where their difference is equal to the given param", () => {
     const givenArray = [1, 7, 5, 9, 2, 12, 3];
     const givenDifference = 2;
     const expectedPairIntegerArray = [{ 1: 3 }, { 3: 5 }, { 5: 7 }, { 7: 9 }];
 
-    expect(calculatePairInteger(givenArray, givenDifference).length).toEqual(4);
+    expect(calculatePairInteger(givenArray, givenDifference).length)
+      .toEqual(expectedPairIntegerArray.length);
   });
 
 });
