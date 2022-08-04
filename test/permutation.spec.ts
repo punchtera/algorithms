9c677abc0deccbd5c37b920b6f96a4e429170074
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-08-04 16:48:03 -0500

    update test design

--- test/permutation.spec.ts
+++ test/permutation.spec.ts
@@ -1,9 +1,13 @@
 import { permutation } from "../src/permutation";
 describe("test permutation function", () => {
+    const consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+
+    beforeEach(() => {
+        consoleSpy.mockClear();
+    });
     it("should call recursively for 'Juan'", () => {
         const givenStr = 'Juan';
-        const expectedNumberOfCalls = 4*3*2*1;
-        console.log = jest.fn();
+        const expectedNumberOfCalls = 4 * 3 * 2 * 1;
 
         permutation(givenStr);
 
