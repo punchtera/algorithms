a3fd81e7b65f160fd892561d8faa4281e920f36e
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-08-30 17:29:48 -0500

    add comments and clean code

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,24 +1,34 @@
-export function calculateFibonacciSequence() {
+/**
+* Function that generates the fibonacci sequence
+* @return   {number[]}      an array with the first 10 elements of the fibonacci sequence
+*/
+export function calculateFibonacciSequence(): number[] {
     const result = [0, 1];
     let acc = 1;
     let prev = 1;
+    const SEQUENCE_LIMIT = 10 - result.length;
 
-    for (let i = 0; i < 8; i++) {
-        result.push(acc)
-        acc += prev
-        prev = result[2 + i]
+    for (let i = 0; i < SEQUENCE_LIMIT; i++) {
+        result.push(acc);
+        acc += prev;
+        prev = result[2 + i];
     }
 
-    return result
+    return result;
 }
 
+/**
+* Function that generates the fibonacci sequence recursively 
+* @return   {Boolean}      result of algoritm if true or false
+*/
 export function calculateFibonacciSequenceRecursively(arr: number[] = [0, 1]): number[] {
-    if (arr.length === 10) {
-        return arr
+    const SEQUENCE_LIMIT = 10;
+    if (arr.length === SEQUENCE_LIMIT) {
+        return arr;
     } else {
-        const last = arr[arr.length - 1]
-        const previous = arr[arr.length - 2]
-        arr.push(last + previous)        
-        return calculateFibonacciSequenceRecursively(arr)
+        const last = arr[arr.length - 1];
+        const previous = arr[arr.length - 2];
+        arr.push(last + previous);
+        return calculateFibonacciSequenceRecursively(arr);
     }
 }
--- src/isUniqueString.ts
+++ src/isUniqueString.ts
@@ -1,9 +1,20 @@
 // Determine if a string has all unique characters, what if you cannot use additional data structures?
+
+/**
+* Function that determines if a string has all unique characters.
+* @param    {String} testWord   the word to test
+* @return   {Boolean}      result of algoritm if true or false
+*/
 export function isUniqueString(testWord: string): boolean {
     const newMap = new Map(testWord.split('').map((character) => [character, character]));
     return newMap.size === testWord.length;
 }
 
+/**
+* Function that determines if a string has all unique characters without using a second data structure (Map).
+* @param    {String} testWord   the word to test
+* @return   {Boolean}      result of algoritm if true or false
+*/
 export function isUniqueStringNoAdditional(testWord: string): boolean {
     for (let index = 0; index < testWord.length; index++) {
         const character = testWord[index];
