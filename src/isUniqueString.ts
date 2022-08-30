fa7d8805ea91d62f547a4a7d8855e01286df5850
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-08-29 19:42:06 -0500

    add isUniqueApproach with no map

--- src/isUniqueString.ts
+++ src/isUniqueString.ts
@@ -1,12 +1,18 @@
 // Determine if a string has all unique characters, what if you cannot use additional data structures?
 export function isUniqueString(testWord: string): boolean {
-    const testMap = new Map();
-    for (let i = 0; i < testWord.length; i++) {
-        testMap.set(testWord[i], i);
-    }
-    return testMap.size === testWord.length;
+    const newMap = new Map(testWord.split('').map((character) => [character, character]));
+    return newMap.size === testWord.length;
 }
 
 export function isUniqueStringNoAdditional(testWord: string): boolean {
-    return false;
+    for (let index = 0; index < testWord.length; index++) {
+        const character = testWord[index];
+        const duplicateWithoutChar = testWord.replace(character, '');
+
+        // O(n^2) as indexof has to loop the string again
+        if (duplicateWithoutChar.indexOf(character) !== - 1) {
+            return false;
+        }
+    }
+    return true;
 }
--- test/isUniqueString.spec.ts
+++ test/isUniqueString.spec.ts
@@ -1,4 +1,4 @@
-import { isUniqueString } from "../src/isUniqueString";
+import { isUniqueString, isUniqueStringNoAdditional } from "../src/isUniqueString";
 describe("isUniqueString", () => {
     it("should return 15 for add(10,5)", () => {
         const givenString = "fake";
@@ -7,5 +7,16 @@ describe("isUniqueString", () => {
     it("should return 5 for add(2,3)", () => {
         const givenString = "aaake";
         expect(isUniqueString(givenString)).toEqual(false);
+    });
+});
+
+describe("isUniqueStringNoAdditional", () => {
+    it("should return 15 for add(10,5)", () => {
+        const givenString = "fake";
+        expect(isUniqueStringNoAdditional(givenString)).toEqual(true);
+    });
+    it("should return 5 for add(2,3)", () => {
+        const givenString = "aaake";
+        expect(isUniqueStringNoAdditional(givenString)).toEqual(false);
     });
 });
