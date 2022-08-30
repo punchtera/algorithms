ccd223d67b79b752e5a76c013b381c099cd32b65
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-08-30 17:22:48 -0500

    Add bucket to check permutation

---
+++ src/checkPermutation.ts
@@ -0,0 +1,12 @@
+/**
+* Function that given two strings, decides if one is a permutation of the other.
+* @param    {String} a    firstString
+* @param    {String} b    secondString
+* @return   {Boolean}      result of algoritm if true or false
+*/
+export function checkPermutation(a: string, b: string): boolean {
+    return false;
+}
+
+
+
