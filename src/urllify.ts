ce10315284a93902414848c1a470ce648795dae5
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-10-10 21:49:32 -0500

    add urlify default test case

---
+++ src/urllify.ts
@@ -0,0 +1,3 @@
+export function urllify(url: string, stringLength: number): string {
+  return `${url}-${stringLength}`;
+}
---
+++ test/urllify.spec.ts
@@ -0,0 +1,12 @@
+// method that replaces all the spaces of a string by '%20', one might assume that the string
+// has additional space at the end to hold the characters, and that you are given the true length of
+// the string
+
+import { urllify } from "../src/urllify";
+
+describe("test urllify functions", () => {
+  const givenUrl = "Mr Jhon Smith     ";
+  const givenLength = 13;
+  const urlifYUrl = urllify(givenUrl, givenLength);
+  expect(urlifYUrl).toEqual("Mr%20Jhon%20Smith");
+});
