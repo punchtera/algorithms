f1beab65a2bd55d57628b1cc82575801ae544db8
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-10-11 20:14:07 -0500

    add problem description

--- src/palindrome.ts
+++ src/palindrome.ts
@@ -1,3 +1,6 @@
+// given a string, write a function to check if it is a permutation of a palindrome.
+// check for spaces and also for duplicated characters
+
 export function isPalindromePermutation(testStr: string) {
   return false;
 }
