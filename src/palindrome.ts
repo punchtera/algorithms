16fdf17467fec4854e239295ed8739edbd491191
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-10-14 18:46:02 -0500

    solve problem and add test case

--- src/palindrome.ts
+++ src/palindrome.ts
@@ -1,6 +1,35 @@
 // given a string, write a function to check if it is a permutation of a palindrome.
 // check for spaces and also for duplicated characters
+// the complexity is O(n) as n is the length of the string
 
 export function isPalindromePermutation(testStr: string) {
+  const testStrNoSpaces = testStr.replace(/\s/g, "").toLocaleLowerCase();
+  const testCharsMap = new Map();
+
+  for (const char of testStrNoSpaces) {
+    if (testCharsMap.has(char) === false) {
+      testCharsMap.set(char, 1);
+    } else {
+      const currentCharValue = testCharsMap.get(char);
+      testCharsMap.set(char, currentCharValue + 1);
+    }
+  }
+
+  let numberOfOdds = 0;
+  for (const keyValue of testCharsMap) {
+    const NUMBER_OF_TIMES_INDEX = 1;
+    const isEven = keyValue[NUMBER_OF_TIMES_INDEX] % 2 === 0;
+
+    if (isEven) {
+      continue;
+    } else {
+      numberOfOdds += 1;
+    }
+  }
+
+  if (numberOfOdds === 1 || numberOfOdds === 0) {
+    return true;
+  }
+
   return false;
 }
--- test/palindrome.spec.ts
+++ test/palindrome.spec.ts
@@ -1,11 +1,27 @@
 import { isPalindromePermutation } from "../src/palindrome";
 
 describe("test palindrome functions", () => {
-  it("should check if the test string matches", () => {
+  it("should return true as the test is a valid palindrome candidate", () => {
     const givenStr = "Tact coa";
 
     const isValid = isPalindromePermutation(givenStr);
 
     expect(isValid).toEqual(true);
+  });
+
+  it("should return true as the test is a valid palindrome candidate with no odd chars", () => {
+    const givenStr = "Tact ca";
+
+    const isValid = isPalindromePermutation(givenStr);
+
+    expect(isValid).toEqual(true);
+  });
+
+  it("should return false as the test is not a valid palindrome candidate", () => {
+    const givenStr = "Tact xcoa";
+
+    const isValid = isPalindromePermutation(givenStr);
+
+    expect(isValid).toEqual(false);
   });
 });
