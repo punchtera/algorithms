8505f6185b6284c14baa08ff684c9c4c7fa64630
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-10-14 19:02:55 -0500

    add placeholder for new test

---
+++ src/oneAway.ts
@@ -0,0 +1,7 @@
+// There are three types of edits that can be performed on strings:
+// insert a character, remove a character or replace a character.
+// given 2 strings, write a function to check if they are one edit away.
+
+export function isOneEditAway(firstString: string, secondString: string) {
+  return false;
+}
---
+++ test/oneAway.spec.ts
@@ -0,0 +1,35 @@
+import { isOneEditAway } from "../src/oneAway";
+
+describe("test oneAway functions", () => {
+  it("should return true as there is one removal away", () => {
+    const givenFirstString = "pale";
+    const givenSecondString = "ple";
+
+    const result = isOneEditAway(givenFirstString, givenSecondString);
+    expect(result).toEqual(true);
+  });
+
+  it("should return true as there is one insert away", () => {
+    const givenFirstString = "pales";
+    const givenSecondString = "ple";
+
+    const result = isOneEditAway(givenFirstString, givenSecondString);
+    expect(result).toEqual(true);
+  });
+
+  it("should return true as there is one replace away", () => {
+    const givenFirstString = "pale";
+    const givenSecondString = "bale";
+
+    const result = isOneEditAway(givenFirstString, givenSecondString);
+    expect(result).toEqual(true);
+  });
+
+  it("should return false as there is two operations away (2 replace)", () => {
+    const givenFirstString = "pale";
+    const givenSecondString = "bake";
+
+    const result = isOneEditAway(givenFirstString, givenSecondString);
+    expect(result).toEqual(false);
+  });
+});
