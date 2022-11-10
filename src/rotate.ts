277396daff7c78fd6a682df7b75ecdf485fb2e0d
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-11-09 21:12:52 -0500

    basic test case

--- src/rotate.ts
+++ src/rotate.ts
@@ -5,24 +5,27 @@
 export function rotateMatrix90Degrees(matrix: number[][]): number[][] {
   const translatedLength = matrix.length;
 
-  const translatedMatrix: number[][] = []
-  for (let i = 0; i < matrix.length; i++) {
-    const element = [];
-    for (let j = 0; j < element.length; j++) {
-      element.push(0);      
-    }
+  const translatedMatrix: number[][] = [];
+  for (let i = 0; i < translatedLength; i++) {
+    const element: [] = [];
     translatedMatrix.push(element);
   }
-  
-  console.log("translated default: ", JSON.stringify(translatedMatrix));
 
   for (let i = 0; i < translatedLength; i++) {
     const columnElement = Number(matrix[0][i]);
     translatedMatrix[i][translatedLength - 1] = Number(columnElement);
   }
 
-  for (let j = 0; j < translatedLength - 1; j++) {
-    const columnElement = Number(matrix[j])  
+  for (let j = 1; j < translatedLength; j++) {
+    const rowElement = Number(matrix[j][translatedLength - 1]);
+    translatedMatrix[translatedLength - 1][translatedLength - 1 - j] =
+      Number(rowElement);
+  }
+
+  for (let k = 1; k < translatedLength; k++) {
+    const columnElement = Number(matrix[translatedLength - 1][0]);
+    translatedMatrix[0][translatedLength - 1 - k] =
+      Number(columnElement);
   }
 
   console.log("translated: ", JSON.stringify(translatedMatrix));
