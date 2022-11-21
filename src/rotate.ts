43ad50795376b1fb5308f6aaa7dea86613e7f1cc
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-11-21 17:20:56 -0500

    2 case passing

--- src/rotate.ts
+++ src/rotate.ts
@@ -4,6 +4,8 @@
 
 export function rotateMatrix90Degrees(matrix: number[][]): number[][] {
   const translatedLength = matrix.length;
+  let elementCount = 0;
+  const numberOfElements = translatedLength * translatedLength;
 
   const translatedMatrix: number[][] = [];
   for (let i = 0; i < translatedLength; i++) {
@@ -14,19 +16,39 @@ export function rotateMatrix90Degrees(matrix: number[][]): number[][] {
   for (let i = 0; i < translatedLength; i++) {
     const columnElement = Number(matrix[0][i]);
     translatedMatrix[i][translatedLength - 1] = Number(columnElement);
+    elementCount += 1;
   }
 
   for (let j = 1; j < translatedLength; j++) {
     const rowElement = Number(matrix[j][translatedLength - 1]);
     translatedMatrix[translatedLength - 1][translatedLength - 1 - j] =
       Number(rowElement);
+    elementCount += 1;
   }
 
   for (let k = translatedLength - 2; k >= 0; k--) {
     const columnElement = Number(matrix[translatedLength - 1][k]);
     translatedMatrix[k][0] = Number(columnElement);
+    elementCount += 1;
   }
 
-  console.log("translated: ", JSON.stringify(translatedMatrix));
+  // if the number of elements have been translated there is no need
+  // of iterating more
+  if (elementCount === numberOfElements) {
+    return translatedMatrix;
+  }
+
+  for (let m = translatedLength - 2; m >= 1; m--) {
+    const rowElement = Number(matrix[m][0]);
+    translatedMatrix[0][m] = Number(rowElement);
+  }
+
+  // After rotating the edges check if the count is complete, if not
+  // prepare submatrix and call recursevely
+  if (numberOfElements - elementCount - 1 === 1) {
+    translatedMatrix[1][1] = matrix[1][1];
+  }
+
+  console.log('1', translatedMatrix);
   return translatedMatrix;
 }
