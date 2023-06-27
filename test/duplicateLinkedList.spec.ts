f1edacc2b754d0450f137e19ef298cb8f16a88d8
Juan Ramirez <juanramirezcastaneda@gmail.com>
2023-06-26 20:05:46 -0500

    call correctly test

--- test/duplicateLinkedList.spec.ts
+++ test/duplicateLinkedList.spec.ts
@@ -1,7 +1,7 @@
 import { LinkedList } from "../src/linkedList";
 
 describe("test the linked list implementation and the duplication algorith", () => {
-  it("should create a linked list", () => {
+  it.skip("should create a linked list", () => {
     const linkedList = new LinkedList(5);
 
     expect(linkedList.head).toBeTruthy();
@@ -13,6 +13,6 @@ describe("test the linked list implementation and the duplication algorith", () 
     linkedList.head.appendToTail(5);
     linkedList.head.appendToTail(3);
 
-    expect(linkedList.removeDuplicates).toEqual(true);
+    expect(linkedList.removeDuplicates()).toEqual(true);
   })
 });
