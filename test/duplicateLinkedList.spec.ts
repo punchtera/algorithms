b32b0ea2fe42725f222b54dff30a71a22cfc2da7
Juan Ramirez <juanramirezcastaneda@gmail.com>
2023-06-28 15:18:25 -0500

    add extra test

--- test/duplicateLinkedList.spec.ts
+++ test/duplicateLinkedList.spec.ts
@@ -7,12 +7,24 @@ describe("test the linked list implementation and the duplication algorith", () 
     expect(linkedList.head).toBeTruthy();
   });
 
-  it("should test if the result of removing duplicates from a linkedlist and return true", () => {
+  it.skip("should test if the result of removing duplicates from a linkedlist and return true", () => {
     const linkedList = new LinkedList(5);
 
     linkedList.head.appendToTail(5);
     linkedList.head.appendToTail(3);
 
     expect(linkedList.removeDuplicates()).toEqual(true);
+    expect(linkedList.getLength()).toEqual(2);
+  });
+
+  it("should test if the result of removing duplicates from a linkedlist with no repeated elements", () => {
+    const linkedList = new LinkedList(5);
+
+    linkedList.head.appendToTail(2);
+    linkedList.head.appendToTail(3);
+
+    expect(linkedList.removeDuplicates()).toEqual(false);
+    linkedList.showValues();
+    expect(linkedList.getLength()).toEqual(3);
   })
 });
