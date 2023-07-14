bc7bdf9914d82c74bc2b66100b665c50eeffe1be
Juan Ramirez <juanramirezcastaneda@gmail.com>
2023-07-14 17:31:54 -0500

    case failing

---
+++ test/linkedList/kthToLast.spec.ts
@@ -0,0 +1,27 @@
+import { LinkedList } from "../../src/linkedList/kthToLast";
+
+describe("test the linked list kthToLast implementation algorith", () => {
+  it("should create a linked list", () => {
+    const linkedList = new LinkedList(5);
+
+    expect(linkedList.head).toBeTruthy();
+  });
+
+/*  Given a Linked List and a number N, write a function that returns 
+    the value at the Nth node from the end of the Linked List. 
+    e.g. 
+    Input: 1 -> 2 -> 3 -> 4, N = 3
+    Output: 2
+
+    Input: 35 -> 15 -> 4 -> 20, N = 4
+    Output: 35  */
+  it("should producte the expected results", () => {
+    const linkedList = new LinkedList(1);
+
+    linkedList.head?.appendToTail(2);
+    linkedList.head?.appendToTail(3);
+    linkedList.head?.appendToTail(4);
+
+    expect(linkedList.findkthToLast(3)).toEqual(2);
+  });
+});
