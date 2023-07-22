fd03ed69d9f630b073dc7f16355b66dbb4bf1e79
Juan Ramirez <juanramirezcastaneda@gmail.com>
2023-07-21 20:49:46 -0500

    fix test headers

--- test/linkedList/deleteMiddleNode.spec.ts
+++ test/linkedList/deleteMiddleNode.spec.ts
@@ -1,13 +1,13 @@
 import { LinkedList } from "../../src/linkedList/deleteMiddleNode";
 
-describe("test the linked list implementation and the duplication algorith", () => {
+describe("test the linked list implementation and the delete middle node algorith", () => {
   it("should create a linked list", () => {
     const linkedList = new LinkedList(5);
 
     expect(linkedList.head).toBeTruthy();
   });
 
-  it("should test if the result of removing duplicates from a linkedlist and return true", () => {
+  it("should remove the middle test from the linked list and return that node", () => {
     const linkedList = new LinkedList(5);
 
     linkedList.head?.appendToTail(5);
