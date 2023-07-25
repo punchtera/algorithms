63920abb6a514dbc9ad69b5363370b3a031af718
Juan Ramirez <juanramirezcastaneda@gmail.com>
2023-07-25 17:03:20 -0500

    base implementation

---
+++ src/linkedList/partition.ts
@@ -0,0 +1,54 @@
+class LinkedListNode {
+    next: LinkedListNode | null = null;
+    value: number;
+
+    constructor(value: number) {
+        this.value = value;
+    }
+
+    appendToTail(newValue: number) {
+        const newLinkListNode = new LinkedListNode(newValue);
+        let n: LinkedListNode = this;
+
+        while (n.next != null) {
+            n = n.next;
+        }
+        n.next = newLinkListNode;
+    }
+}
+
+export class LinkedList {
+    head: LinkedListNode | null
+
+    constructor(headValue: number) {
+        const headNode = new LinkedListNode(headValue);
+        this.head = headNode;
+    }
+
+    showValues() {
+        let n: LinkedListNode | null = this.head;
+
+        console.log('start print');
+        while (n != null) {
+            console.log(`Node Value - ${n.value}`);
+            n = n.next;
+        }
+        console.log('finish print');
+    }
+
+    getLength() {
+        let n: LinkedListNode | null = this.head;
+        let length = 0;
+
+        while (n != null) {
+            length += 1;
+            n = n.next;
+        }
+
+        return length;
+    }
+
+    partition(): null {
+        return null;
+    }
+}
